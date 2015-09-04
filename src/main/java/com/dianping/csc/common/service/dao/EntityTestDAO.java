9d464d300512b8ed95af973bdf23ec10f262707b
csophys <zjsy_cs@163.com>
2015-09-04 18:13:06 +0800

    sqlmap

---
+++ src/main/java/com/dianping/csc/common/service/dao/EntityTestDAO.java
@@ -0,0 +1,11 @@
+package com.dianping.csc.common.service.dao;
+
+import com.dianping.csc.common.service.dao.DAO;
+import com.dianping.csc.common.service.entity.EntityTest;
+
+/**
+* Created by csophys with template on 2015-9-4.
+*/
+public interface EntityTestDAO extends DAO<EntityTest> {
+
+}
