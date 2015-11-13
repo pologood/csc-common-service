fd1bd0ede52bebe8950216acc82764795fd7b035
csophys <zjsy_cs@163.com>
2015-11-13 11:52:30 +0800

    add spring test base

---
+++ src/main/java/com/dianping/csc/common/service/util/SpringBaseWithCustomizeRunnerTest.java
@@ -0,0 +1,22 @@
+package com.dianping.csc.common.service.util;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+
+/**
+ * Created by yuchao on 15/4/16.
+ */
+@RunWith(CustomizeJunit4ClassRunner.class)
+@ContextConfiguration(locations = {
+        "classpath*:/config/spring/local/appcontext-*.xml"
+})
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class
+        })
+public class SpringBaseWithCustomizeRunnerTest {
+}
